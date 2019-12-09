97465d68cbf0e845273267053a0ca1ea689050a5
proochster <proochster@gmail.com>
2019-12-09 11:46:05 +0000

    + Theme nav menus

--- wp-content/themes/wp-base/functions.php
+++ wp-content/themes/wp-base/functions.php
@@ -22,136 +22,136 @@
  * THEME CUSTOM SETUP
  */
 
-function wpbase_theme_setup()
-{
+    function wpbase_theme_setup()
+    {
 
-    /**
-     * Feed links
-     * @link https://codex.wordpress.org/Automatic_Feed_Links
-     */
-    // add_theme_support('automatic-feed-links');
+        /**
+         * Feed links
+         * @link https://codex.wordpress.org/Automatic_Feed_Links
+         */
+        // add_theme_support('automatic-feed-links');
 
-    /**
-     * Title tag support
-     * @link https://codex.wordpress.org/Title_Tag
-     */
-    // add_theme_support('title-tag');
+        /**
+         * Title tag support
+         * @link https://codex.wordpress.org/Title_Tag
+         */
+        add_theme_support('title-tag');
 
-    /**
-     * Custiomize refresh
-     * @link https://make.wordpress.org/core/2016/03/22/implementing-selective-refresh-support-for-widgets/
-     */
-    add_theme_support('customize-selective-refresh-widgets');
+        /**
+         * Custiomize refresh
+         * @link https://make.wordpress.org/core/2016/03/22/implementing-selective-refresh-support-for-widgets/
+         */
+        add_theme_support('customize-selective-refresh-widgets');
 
-    /**
-     * HTML5 support
-     * @link https://codex.wordpress.org/Theme_Markup
-     */
-    add_theme_support(
-        'html5',
-        array(
-            'comment-list',
-            'comment-form',
-            'search-form',
-            'gallery',
-            'caption'
-        )
-    );
+        /**
+         * HTML5 support
+         * @link https://codex.wordpress.org/Theme_Markup
+         */
+        add_theme_support(
+            'html5',
+            array(
+                'comment-list',
+                'comment-form',
+                'search-form',
+                'gallery',
+                'caption'
+            )
+        );
 
-    /**
-     * Wide alignement
-     * @link https://developer.wordpress.org/block-editor/developers/themes/theme-support/
-     */
-    // add_theme_support( 'align-wide' );
+        /**
+         * Wide alignement
+         * @link https://developer.wordpress.org/block-editor/developers/themes/theme-support/
+         */
+        // add_theme_support( 'align-wide' );
 
-    /*
-	* Custom background color.
-    * @link https://codex.wordpress.org/Custom_Backgrounds
-    */
-    add_theme_support(
-        'custom-background',
-        array(
-            'default-color'          => '',
-            'default-image'          => '',
-            'default-repeat'         => 'repeat',
-            'default-position-x'     => 'left',
-            'default-position-y'     => 'top',
-            'default-size'           => 'auto',
-            'default-attachment'     => 'scroll',
-            'wp-head-callback'       => '_custom_background_cb',
-            'admin-head-callback'    => '',
-            'admin-preview-callback' => ''
-        )
-    );
+        /*
+        * Custom background color.
+        * @link https://codex.wordpress.org/Custom_Backgrounds
+        */
+        add_theme_support(
+            'custom-background',
+            array(
+                'default-color'          => '#CCCCCC',
+                // 'default-image'          => '',
+                // 'default-repeat'         => 'repeat',
+                // 'default-position-x'     => 'left',
+                // 'default-position-y'     => 'top',
+                // 'default-size'           => 'auto',
+                // 'default-attachment'     => 'scroll',
+                // 'wp-head-callback'       => '_custom_background_cb',
+                // 'admin-head-callback'    => '',
+                // 'admin-preview-callback' => ''
+            )
+        );
 
-    /**
-     * Theme logo support
-     * @link https://codex.wordpress.org/Theme_Logo
-     */
-    add_theme_support(
-        'custom-logo',
-        array(
-            // 'height'      => 50,
-            // 'width'       => 50,
-            // 'flex-height' => true,
-            // 'flex-width'  => true,
-            'header-text' => array('site-title', 'site-description'),
-        )
-    );
+        /**
+         * Theme logo support
+         * @link https://codex.wordpress.org/Theme_Logo
+         */
+        add_theme_support(
+            'custom-logo',
+            array(
+                // 'height'      => 50,
+                // 'width'       => 50,
+                // 'flex-height' => true,
+                // 'flex-width'  => true,
+                'header-text' => array('site-title', 'site-description'),
+            )
+        );
 
-    /**
-     * Custon header support
-     * @link https://codex.wordpress.org/Custom_Headers
-     */
-    add_theme_support(
-        'custom-header',
-        array(
-            'default-image'          => '',
-            'width'                  => 0,
-            'height'                 => 0,
-            'flex-height'            => false,
-            'flex-width'             => false,
-            'uploads'                => true,
-            'random-default'         => false,
-            'header-text'            => true,
-            'default-text-color'     => '',
-            'wp-head-callback'       => '',
-            'admin-head-callback'    => '',
-            'admin-preview-callback' => '',
-        )
-    );
+        /**
+         * Custon header support
+         * @link https://codex.wordpress.org/Custom_Headers
+         */
+        add_theme_support(
+            'custom-header',
+            array(
+                'default-image'          => '',
+                'width'                  => 0,
+                'height'                 => 0,
+                'flex-height'            => false,
+                'flex-width'             => false,
+                'uploads'                => true,
+                'random-default'         => false,
+                'header-text'            => true,
+                'default-text-color'     => '',
+                'wp-head-callback'       => '',
+                'admin-head-callback'    => '',
+                'admin-preview-callback' => '',
+            )
+        );
 
-    /**
-     * Enable support for Post Thumbnails on posts and pages.
-     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-     */
-    add_theme_support(
-        'post-thumbnails'
-    );
+        /**
+         * Enable support for Post Thumbnails on posts and pages.
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support(
+            'post-thumbnails'
+        );
 
 
-    /**
-     * Post formats
-     * @link https://wordpress.org/support/article/post-formats/
-     */
-    // add_theme_support(
-    //     'post-formats',
-    //     array(
-    //         'aside',
-    //         'gallery',
-    //         'link',
-    //         'image',
-    //         'quote',
-    //         'ststus',
-    //         'video',
-    //         'audio',
-    //         'chat',
-    //     )
-    // );
+        /**
+         * Post formats
+         * @link https://wordpress.org/support/article/post-formats/
+         */
+        // add_theme_support(
+        //     'post-formats',
+        //     array(
+        //         'aside',
+        //         'gallery',
+        //         'link',
+        //         'image',
+        //         'quote',
+        //         'ststus',
+        //         'video',
+        //         'audio',
+        //         'chat',
+        //     )
+        // );
 
-}
+    }
 
-add_action('after_setup_theme', 'wpbase_theme_setup');
+    add_action('after_setup_theme', 'wpbase_theme_setup');
 
 /**
  * END THEME CUSTOM SETUP
@@ -159,51 +159,79 @@ add_action('after_setup_theme', 'wpbase_theme_setup');
  */
 
 /**
+ * ---------------
+ * MENUS
+ */
+
+    /**
+     * Register theme menus
+     */
+    add_action( 'after_setup_theme', 'register_theme_menus' );
+    
+    function register_theme_menus() {
+        register_nav_menus( array(
+            'header_menu_left' => __( 'Header left', 'wpbase' ),
+            'header_menu_right' => __( 'Header right', 'wpbase' ),
+        ) );
+    }
+
+    /**
+     * Append navbar classes to the anchors. Only apply to selected menus
+     */
+    function add_menu_item_class($ulclass, $args) {
+        if($args->theme_location == 'header_menu_left' || $args->theme_location == 'header_menu_right'){
+            return preg_replace('/<a /', '<a class="navbar-item"', $ulclass);
+        }
+    }
+    add_filter('wp_nav_menu','add_menu_item_class', 1, 2);
+
+
+/**
  * ------------------
  * CUSTOM POST TYPES
  */
 
-// add_action('init', 'create_post_type');
-// // REGITSTER NEW POST TYPES
-// function create_post_type()
-// {
-//     // PROJECTS / WORK
-//     register_post_type(
-//         'work_post', // Post Type cannot be named the same as the page
-//         array(
-//             'labels' => array(
-//                 'name' => __('Work'),
-//                 'singular_name' => __('Work'),
-//                 'add_new' => __('Add new'),
-//                 'add_new_item' => __('Add new work item'),
-//                 'edit_item' => __('Edit work item'),
-//                 'new_item' => __('New work item'),
-//                 'view_item' => __('View work item'),
-//                 'search_items' => __('Search work item'),
-//                 'not_found' =>  __('Nothing found'),
-//                 'not_found_in_trash' => __('Nothing found in Trash'),
-//                 'parent_item_colon' => ''
-//             ),
-//             'public' => true,
-//             'exclude_from_search' => false,
-//             'has_archive' => false,
-//             'publicly_queryable' => true,
-//             'hierarchical' => true,
-//             'menu_icon' => 'dashicons-hammer',
-//             'supports' => array(
-//                 'title',
-//                 'editor',
-//                 'revisions',
-//                 'author',
-//             ),
-//             'taxonomies' => array(
-//                 'post_tag',
-//                 'work'
-//             ),
-//             'rewrite' => array('slug' => 'my-work')
-//         )
-//     );
-// }
+    // add_action('init', 'create_post_type');
+    // // REGITSTER NEW POST TYPES
+    // function create_post_type()
+    // {
+    //     // PROJECTS / WORK
+    //     register_post_type(
+    //         'work_post', // Post Type cannot be named the same as the page
+    //         array(
+    //             'labels' => array(
+    //                 'name' => __('Work'),
+    //                 'singular_name' => __('Work'),
+    //                 'add_new' => __('Add new'),
+    //                 'add_new_item' => __('Add new work item'),
+    //                 'edit_item' => __('Edit work item'),
+    //                 'new_item' => __('New work item'),
+    //                 'view_item' => __('View work item'),
+    //                 'search_items' => __('Search work item'),
+    //                 'not_found' =>  __('Nothing found'),
+    //                 'not_found_in_trash' => __('Nothing found in Trash'),
+    //                 'parent_item_colon' => ''
+    //             ),
+    //             'public' => true,
+    //             'exclude_from_search' => false,
+    //             'has_archive' => false,
+    //             'publicly_queryable' => true,
+    //             'hierarchical' => true,
+    //             'menu_icon' => 'dashicons-hammer',
+    //             'supports' => array(
+    //                 'title',
+    //                 'editor',
+    //                 'revisions',
+    //                 'author',
+    //             ),
+    //             'taxonomies' => array(
+    //                 'post_tag',
+    //                 'work'
+    //             ),
+    //             'rewrite' => array('slug' => 'my-work')
+    //         )
+    //     );
+    // }
 
 
 
@@ -212,20 +240,20 @@ add_action('after_setup_theme', 'wpbase_theme_setup');
  * WIDGET PLACEHOLDERS
  */
 
-add_action( 'widgets_init', 'wpbase_widgets_init' );
-// // REGITER SIDEBARS
-function wpbase_widgets_init() {
+    add_action( 'widgets_init', 'wpbase_widgets_init' );
+    // // REGITER SIDEBARS
+    function wpbase_widgets_init() {
 
-  	register_sidebar( array(
-  		'name'          => __( 'Footer', 'wpbase' ),
-  		'id'            => 'footer__widgets',
-  		'description'   => __( 'Appears in the footer of the page in the 1st section', 'wpbase' ),
-  		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-  		'after_widget'  => '</section>',
-  		'before_title'  => '<h3 class="widget-title">',
-  		'after_title'   => '</h3>',
-  	) );
-  }
+        register_sidebar( array(
+            'name'          => __( 'Footer', 'wpbase' ),
+            'id'            => 'footer__widgets',
+            'description'   => __( 'Appears in the footer of the page in the 1st section', 'wpbase' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        ) );
+    }
 
 
 /**
@@ -233,53 +261,53 @@ function wpbase_widgets_init() {
  * ENQUEUE SCRIPTS
  */
 
-// add_action( 'wp_enqueue_scripts', 'wp2018_scripts' );
-//  // LOAD SCRIPTS
-//  function wp2018_scripts() {
+    // add_action( 'wp_enqueue_scripts', 'wp2018_scripts' );
+    //  // LOAD SCRIPTS
+    //  function wp2018_scripts() {
 
-//     // Owl carousel
-//     // wp_enqueue_script( 'wp2018-owl-script', '//cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js', array('jquery'), '', false );
-//     // Plugins
-//   	// wp_enqueue_script( 'wp2018-plugins-script', get_template_directory_uri() . '/js/jquery.matchHeight.min.js', array('jquery'), '', true );
-//     // App.js
-//     wp_enqueue_script( 'wp2018-app-script', get_template_directory_uri() . '/js/app-min.js', array('jquery'), '', true );
+    //     // Owl carousel
+    //     // wp_enqueue_script( 'wp2018-owl-script', '//cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js', array('jquery'), '', false );
+    //     // Plugins
+    //   	// wp_enqueue_script( 'wp2018-plugins-script', get_template_directory_uri() . '/js/jquery.matchHeight.min.js', array('jquery'), '', true );
+    //     // App.js
+    //     wp_enqueue_script( 'wp2018-app-script', get_template_directory_uri() . '/js/app-min.js', array('jquery'), '', true );
 
-//     // Theme stylesheet.
-//     wp_enqueue_style( 'wp2018-style', get_stylesheet_uri() );
-//     // Add Wordpress dashicons to the theme
-//     wp_enqueue_style( 'dashicons' );
-//  }
-
-
-//   // Defer scripts
-//   // add_filter( 'clean_url', function( $url )
-//   // {
-//   //     if ( FALSE === strpos( $url, '.js' ) )
-//   //     { // not our file
-//   //         return $url;
-//   //     }
-//   //     // Must be a ', not "!
-//   //     return "$url' defer='defer";
-//   // }, 11, 1 );
+    //     // Theme stylesheet.
+    //     wp_enqueue_style( 'wp2018-style', get_stylesheet_uri() );
+    //     // Add Wordpress dashicons to the theme
+    //     wp_enqueue_style( 'dashicons' );
+    //  }
 
 
-/* Scripts*/
-if (!is_admin()) add_action("wp_enqueue_scripts", "tidy_scripts", 11);
-function tidy_scripts() {
-    wp_dequeue_style( 'wp-block-library' );
-    wp_deregister_script('wp-embed');
-    wp_deregister_script('jquery');
-    wp_enqueue_script('jquery');
-}
+    //   // Defer scripts
+    //   // add_filter( 'clean_url', function( $url )
+    //   // {
+    //   //     if ( FALSE === strpos( $url, '.js' ) )
+    //   //     { // not our file
+    //   //         return $url;
+    //   //     }
+    //   //     // Must be a ', not "!
+    //   //     return "$url' defer='defer";
+    //   // }, 11, 1 );
 
-// https://www.isitwp.com/remove-code-wordpress-header/
-remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'wp_generator'); // WordPress version
-remove_action('wp_head', 'start_post_rel_link');
-remove_action('wp_head', 'index_rel_link');
-remove_action('wp_head', 'adjacent_posts_rel_link');
 
-/* Disable WP emojis */
-remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    /* Scripts*/
+    if (!is_admin()) add_action("wp_enqueue_scripts", "tidy_scripts", 11);
+    function tidy_scripts() {
+        wp_dequeue_style( 'wp-block-library' );
+        wp_deregister_script('wp-embed');
+        wp_deregister_script('jquery');
+        wp_enqueue_script('jquery');
+    }
+
+    // https://www.isitwp.com/remove-code-wordpress-header/
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'wp_generator'); // WordPress version
+    remove_action('wp_head', 'start_post_rel_link');
+    remove_action('wp_head', 'index_rel_link');
+    remove_action('wp_head', 'adjacent_posts_rel_link');
+
+    /* Disable WP emojis */
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
