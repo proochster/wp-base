ba892ea7f09f014cc319b2c78bf6f6f42c0d169d
proochster <proochster@gmail.com>
2019-12-09 22:26:22 +0000

    + Basic footer

--- wp-content/themes/wp-base/footer.php
+++ wp-content/themes/wp-base/footer.php
@@ -8,8 +8,12 @@
  */
 ?>
 </main>
-<footer>
-Footer
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong><?php bloginfo('title') ?></strong> by <a href="https://jgthms.com">Jakub Prochniak</a>. GitHub <a href="https://github.com/proochster/wp-base" title="GitHub repository">repository</a>.
+    </p>
+  </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
