5c17bce81eb11b3d67ddcec4a64f87b34d4344cd
proochster <proochster@gmail.com>
2019-12-10 17:00:12 +0000

    + Body scroll lock

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,12 +1,14 @@
 const TopNav = {
     $button: document.querySelector('.navbar-burger'),
     $nav: document.querySelector('.navbar-menu'),
+    $body: document.querySelector('body'),
 
     init: function() {
         const self = this;
 
         this.$button.onclick = function(){
             self.$nav.classList.toggle('is-active');
+            self.$body.classList.toggle('no-scroll');
             // Update aria attribute
             this.getAttribute('aria-expanded') === 'false' ? this.setAttribute('aria-expanded', 'true') : this.setAttribute('aria-expanded', 'false');
         };
