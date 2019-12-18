f9ad382695e22611d9e0589f0b368c76d2976ae6
proochster <proochster@gmail.com>
2019-12-18 10:11:43 +0000

    ^ Home theme fluff removed

--- wp-content/themes/wp-base/home.php
+++ wp-content/themes/wp-base/home.php
@@ -11,24 +11,6 @@
 get_header();
 ?>
 
-<?php // Only display the custom header imege on the front page.
-if ( get_header_image() && is_front_page() ) : ?>
-
-    <figure class="image">
-        <img alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" src="<?php echo get_header_image(); ?>">
-    </figure>
-
-<?php endif ?>
-
-<!-- <section class="hero is-warning is-bold is-medium">
-    <div class="hero-body">
-        <div class="container">
-            <h1 class="title">Primary title</h1>
-            <h2 class="subtitle">Primary subtitle</h2>
-        </div>
-    </div>
-</section> -->
-
 <div class="section">
     <div class="container">
 
