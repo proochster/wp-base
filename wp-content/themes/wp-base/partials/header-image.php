bfd708eb262306cf66c0009cdf77b9eb62b6a163
proochster <proochster@gmail.com>
2019-12-24 12:27:50 +0000

    ^ Header image lazyload

--- wp-content/themes/wp-base/partials/header-image.php
+++ wp-content/themes/wp-base/partials/header-image.php
@@ -2,7 +2,7 @@
 if ( get_header_image() && is_front_page() ) : ?>
 
     <figure class="image">
-        <img alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" src="<?php echo get_header_image(); ?>">
+        <img alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" src="<?php echo get_header_image(); ?>" class="lazy">
     </figure>
 
 <?php endif ?>
